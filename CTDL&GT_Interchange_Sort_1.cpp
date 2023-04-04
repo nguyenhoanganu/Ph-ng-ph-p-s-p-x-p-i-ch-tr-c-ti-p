f71ffd65b19eda5cc9b292b79f84375e69392c45
nguyenhoanganu <129148984+nguyenhoanganu@users.noreply.github.com>
2023-04-04 15:07:57 +0700

    Update CTDL&GT_Interchange_Sort_1.cpp

--- CTDL&GT_Interchange_Sort_1.cpp
+++ CTDL&GT_Interchange_Sort_1.cpp
@@ -1,13 +1,34 @@
-https://chidokun.github.io/2015/03/interchange-sort/
+
+ 
+
+ https://tuicocach.com/interchange-sort-thuat-toan-sap-xep-doi-cho-truc-tiep/
 
 
-void InterchangeSort(int *a, int n)
+#include <stdio.h>
+void Swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void InterchangeSort(int a[], int n){   
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if(a[i] > a[j])
+                Swap(a[i], a[j]);
+}
+void printArray(int arr[], int size)
 {
-   for (int i = 0; i < n-1; i++)
-      for (int j = i+1; j < n; j++)
-         if (a[i] > a[j])
-         {
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-         }
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main()
+{
+    int arr[] = {3,4,1,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    InterchangeSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
